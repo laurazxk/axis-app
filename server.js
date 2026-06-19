e881e1ce6c24b6a7b1be04f35e0a1e143b9c9033
laurazxk <lauramarques2405@gmail.com>
2026-06-18 22:39:07 -0300

    backend API

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
 
+// Rota raiz da API
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.json({ 
+    status: 'online',
+    message: 'Axis API rodando com sucesso!'
+  });
 });
 
 const PORT = process.env.PORT || 3000;
